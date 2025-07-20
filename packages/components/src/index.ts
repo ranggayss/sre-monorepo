0c8c94c532937759090c57d11f8bef906d1f5f81
ranggays <ranggayoviesaputra@gmail.com>
2025-07-20 15:19:19 +0700

    add shared components

---
+++ packages/components/src/index.ts
@@ -0,0 +1,4 @@
+// Export semua komponen yang ingin Anda expose
+export { DashboardLayout } from './DashboardLayout';
+export { DashboardHeader } from './DashboardHeader';
+export { DashboardNavbar } from './DashboardNavbar';
